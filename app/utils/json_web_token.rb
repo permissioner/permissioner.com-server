084c4e685f51c85da94861e1f86c54ba00036b91
Nicolas Autzen <n.autzen@gmail.com>
2019-07-23 10:24:37 +0200

    remove unnecessary class variable

--- app/utils/json_web_token.rb
+++ app/utils/json_web_token.rb
@@ -2,15 +2,13 @@ require 'jwt'
 
 class JsonWebToken
 
-  @@hmac_secret = ENV['JWT_SECRET']
-
   def encode(payload)
-    token = JWT.encode payload, @@hmac_secret, 'HS256'
+    token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
   end
 
   def decode(jwt)
     if jwt && jwt.split('.').length == 3
-      decoded_token = JWT.decode jwt, @@hmac_secret, true, { algorithm: 'HS256' }
+      decoded_token = JWT.decode jwt, ENV['JWT_SECRET'], true, { algorithm: 'HS256' }
       decoded_token[0]
     end
   end
